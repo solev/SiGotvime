34e7b0f1437d41efd48f1e18cabf9cf981ff7757
Martin Solev <martin.solev@tripx.se>
2020-12-25 15:52:14 +0100

    remove an old project fix some errors

--- SiGotvime (Admin)/Controllers/RecipeController.cs
+++ SiGotvime (Admin)/Controllers/RecipeController.cs
@@ -18,7 +18,7 @@ namespace Food_App_Service.Controllers
 {
     [RoutePrefix("api/Recipe")]
     public class RecipeController : ApiController
-    {        
+    {
         IRecipeRepository recipeRepository;
         FoodDatabase db;
         public RecipeController(RecipeRepository _recipeRepository, FoodDatabase _db)
@@ -31,16 +31,16 @@ namespace Food_App_Service.Controllers
         [HttpPost]
         public IEnumerable<RecipeViewModel> search(SearchModel model)
         {
-            
+
             Dictionary<Recipe, int> recipes = new Dictionary<Recipe, int>();
             List<RecipeViewModel> result = new List<RecipeViewModel>();
-            
-            foreach(var t in model.ingredients)
+
+            foreach (var t in model.ingredients)
             {
-                var tempList = db.IngredientsInRecipe.Where(x=> x.Ingredient.ID == t).Select(x=>x.Recipe).ToList();
-                foreach(var r in tempList)
+                var tempList = db.IngredientsInRecipe.Where(x => x.Ingredient.ID == t).Select(x => x.Recipe).ToList();
+                foreach (var r in tempList)
                 {
-                    if(!recipes.ContainsKey(r))
+                    if (!recipes.ContainsKey(r))
                     {
                         recipes.Add(r, 1);
                     }
@@ -52,22 +52,22 @@ namespace Food_App_Service.Controllers
             }
 
             int take = 5;
-            if(model.taken == 0)
+            if (model.taken == 0)
                 take = 15;
 
 
             var sortedRecipes = recipes.OrderByDescending(x => x.Value).Skip(model.taken).Take(take).ToList();
 
-            foreach(var item in sortedRecipes)
+            foreach (var item in sortedRecipes)
             {
                 RecipeViewModel vm = new RecipeViewModel { Content = item.Key.Content, ImageUrl = CommonHelper.createImageUrl(item.Key.ImageUrl), CroppedUrl = item.Key.CroppedUrl != null ? CommonHelper.createImageUrl(item.Key.CroppedUrl) : null, PreparationTime = item.Key.PreparationTime, Title = item.Key.Title, Rating = item.Key.Rating };
                 var list = db.IngredientsInRecipe.Where(x => x.Recipe.ID == item.Key.ID).ToList();
                 List<IngredientsRecipe> ordered = new List<IngredientsRecipe>();
 
-                foreach(var ing in list)
+                foreach (var ing in list)
                 {
                     var t = model.ingredients.FirstOrDefault(x => x == ing.Ingredient.ID);
-                    if(t!=null)
+                    if (t != null)
                     {
                         ordered.Add(ing);
                     }
@@ -77,17 +77,17 @@ namespace Food_App_Service.Controllers
                 ordered.AddRange(list);
                 List<IngredientViewModel> tempList = new List<IngredientViewModel>();
 
-                foreach(var ing in ordered)
+                foreach (var ing in ordered)
                 {
                     tempList.Add(new IngredientViewModel { Name = ing.Ingredient.Name, Quantity = ing.Quantity });
                 }
                 vm.ingredients = tempList;
                 result.Add(vm);
-                
-            } 
+
+            }
             return result;
-        }            
-               
+        }
+
 
         [Route("GetByCategory")]
         [HttpPost]
@@ -95,31 +95,31 @@ namespace Food_App_Service.Controllers
         {
             var res = recipeRepository.GetByCategory(model.Categories[0], model.taken / 15);
 
-            foreach(var item in res)
+            foreach (var item in res)
             {
                 item.ImageUrl = CommonHelper.createImageUrl(item.ImageUrl);
-                if(item.CroppedUrl != null)
+                if (item.CroppedUrl != null)
                 {
                     item.CroppedUrl = CommonHelper.createImageUrl(item.CroppedUrl);
-                }                
+                }
             }
             return res;
         }
 
         [Route("GetHomeRecipes")]
         [HttpPost]
-        public IEnumerable<RecipeViewModel> getThem([FromBody] int taken)
+        public IEnumerable<SiGotvime.Data.DTO.RecipeDto> getThem([FromBody] int taken)
         {
             string baseUrl = Constants.rootUrl;
             var recipes = recipeRepository.GetLatest((taken / Constants.RecipesPerPage) + 1);
-            
-            List<RecipeViewModel> res = new List<RecipeViewModel>();
-            foreach(var item in recipes)
+
+            List<SiGotvime.Data.DTO.RecipeDto> res = new List<SiGotvime.Data.DTO.RecipeDto>();
+            foreach (var item in recipes)
             {
-                var r = recipeRepository.GetFullRecipeById(item);
+                SiGotvime.Data.DTO.RecipeDto r = recipeRepository.GetFullRecipeById(item);
                 r.ID = item;
                 r.ImageUrl = CommonHelper.createImageUrl(r.ImageUrl);
-                if(r.CroppedUrl != null)
+                if (r.CroppedUrl != null)
                 {
                     r.CroppedUrl = CommonHelper.createImageUrl(r.CroppedUrl);
                 }
@@ -130,20 +130,20 @@ namespace Food_App_Service.Controllers
 
         [Route("GetEasiest")]
         [HttpPost]
-        public IEnumerable<RecipeViewModel> getEeasy([FromBody]int taken)
+        public IEnumerable<RecipeViewModel> getEeasy([FromBody] int taken)
         {
-            var result = recipeRepository.GetEasiest((taken/Constants.RecipesPerPage) +1);
-            foreach(var item in result)
+            var result = recipeRepository.GetEasiest((taken / Constants.RecipesPerPage) + 1);
+            foreach (var item in result)
             {
                 item.ImageUrl = CommonHelper.createImageUrl(item.ImageUrl);
-                if(item.CroppedUrl!=null)
+                if (item.CroppedUrl != null)
                 {
                     item.CroppedUrl = CommonHelper.createImageUrl(item.CroppedUrl);
                 }
             }
             return result;
         }
-          
+
 
     }
 }
